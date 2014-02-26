2bbefacf45f56db9f4e4400e8d074f176309445d
OdetteDu <Odette.Du@rice.edu>
2014-02-25 18:09:27 -0800

    Add argument names

--- yalnix.c
+++ yalnix.c
@@ -1,12 +1,12 @@
 #include <comp421/yalnix.h>
 #include <comp421/hardware.h>
 
-extern int SetKernelBrk(void *)
+extern int SetKernelBrk(void *addr)
 {
 
 }
 
-extern void KernelStart(ExceptionStackFrame *, unsigned int, void *, char **)
+extern void KernelStart(ExceptionStackFrame *frame, unsigned int pmem_size, void *orig_brk, char **cmd_args)
 {
 
 }
@@ -16,17 +16,17 @@ extern int Fork(void)
 
 }
 
-extern int Exec(char *, char **)
+extern int Exec(char *filename, char **argvec)
 {
 
 }
 
-extern void Exit(int) __attribute__ ((noreturn))
+extern void Exit(int status) __attribute__ ((noreturn))
 {
 
 }
 
-extern int Wait(int *)
+extern int Wait(int *status_ptr)
 {
 
 }
@@ -36,22 +36,22 @@ extern int GetPid(void)
 
 }
 
-extern int Brk(void *)
+extern int Brk(void *addr)
 {
 
 }
 
-extern int Delay(int)
+extern int Delay(int clock_ticks)
 {
 
 }
 
-extern int TtyRead(int, void *, int)
+extern int TtyRead(int tty_id, void *buf, int len)
 {
 
 }
 
-extern int TtyWrite(int, void *, int)
+extern int TtyWrite(int tty_id, void *buf, int len)
 {
 
 }
