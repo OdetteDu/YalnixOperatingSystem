3130c1d8357b8526541dd4d499f8ba4c201eb213
OdetteDu <Odette.Du@rice.edu>
2014-02-25 20:18:26 -0600

    Finally make it compiled

--- yalnix.c
+++ yalnix.c
@@ -3,7 +3,7 @@
 
 extern int SetKernelBrk(void *addr)
 {
-
+	return 0;
 }
 
 extern void KernelStart(ExceptionStackFrame *frame, unsigned int pmem_size, void *orig_brk, char **cmd_args)
@@ -13,47 +13,47 @@ extern void KernelStart(ExceptionStackFrame *frame, unsigned int pmem_size, void
 
 extern int Fork(void)
 {
-
+	return 0;
 }
 
 extern int Exec(char *filename, char **argvec)
 {
-
+	return 0;
 }
 
-extern void Exit(int status) __attribute__ ((noreturn))
+extern void Exit(int status)
 {
-
+	
 }
 
 extern int Wait(int *status_ptr)
 {
-
+	return 0;
 }
 
 extern int GetPid(void)
 {
-
+	return 0;
 }
 
 extern int Brk(void *addr)
 {
-
+	return 0;
 }
 
 extern int Delay(int clock_ticks)
 {
-
+	return 0;
 }
 
 extern int TtyRead(int tty_id, void *buf, int len)
 {
-
+	return 0;
 }
 
 extern int TtyWrite(int tty_id, void *buf, int len)
 {
-
+	return 0;
 }
 
 
