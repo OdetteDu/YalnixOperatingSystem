d07b34be26da5830aa12255318ecfcb6bdaee404
OdetteDu <Odette.Du@rice.edu>
2014-03-10 14:40:27 -0500

    Combine two files

--- interruptVectorTable.c
+++ interruptVectorTable.c
@@ -1,39 +1,4 @@
 #include <comp421/hardware.h>
 #include <comp421/yalnix.h>
 
-void (*interruptTable[TRAP_VECTOR_SIZE])(ExceptionStackFrame *);
 
-void trapKernel(ExceptionStackFrame *exceptionStackFrame)
-{
-	  
-}
-
-void trapClock(ExceptionStackFrame *exceptionStackFrame)
-{
-	  
-}
-
-void trapIllegal(ExceptionStackFrame *exceptionStackFrame)
-{
-	  
-}
-
-void trapMemory(ExceptionStackFrame *exceptionStackFrame)
-{
-	  
-}
-
-void trapMath(ExceptionStackFrame *exceptionStackFrame)
-{
-	  
-}
-
-void trapTTYReceive(ExceptionStackFrame *exceptionStackFrame)
-{
-	  
-}
-
-void trapTTYTransmit(ExceptionStackFrame *exceptionStackFrame)
-{
-	  
-}
--- yalnix.c
+++ yalnix.c
@@ -1,6 +1,42 @@
 #include <comp421/yalnix.h>
 #include <comp421/hardware.h>
 
+void (*interruptTable[TRAP_VECTOR_SIZE])(ExceptionStackFrame *);
+
+void trapKernel(ExceptionStackFrame *exceptionStackFrame)
+{
+    
+}
+
+void trapClock(ExceptionStackFrame *exceptionStackFrame)
+{
+    
+}
+
+void trapIllegal(ExceptionStackFrame *exceptionStackFrame)
+{
+    
+}
+
+void trapMemory(ExceptionStackFrame *exceptionStackFrame)
+{
+    
+}
+
+void trapMath(ExceptionStackFrame *exceptionStackFrame)
+{
+    
+}
+
+void trapTTYReceive(ExceptionStackFrame *exceptionStackFrame)
+{
+    
+}
+
+void trapTTYTransmit(ExceptionStackFrame *exceptionStackFrame)
+{
+    
+}
 
 extern int SetKernelBrk(void *addr)
 {
